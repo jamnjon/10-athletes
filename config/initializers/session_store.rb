2ed8ee5c5f78d3f10fcb522b578b1611b44f420b
Jon Mendelson <mendelsonja@gmail.com>
2020-12-01 14:51:54 -0800

    changed session store to front end, not back end

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
 if Rails.env === 'production'
-  Rails.application.config.session_store :cookie_store, key: '_10-athletes', domain: 'https://tenathletes.herokuapp.com'
+  Rails.application.config.session_store :cookie_store, key: '_10-athletes', domain: 'https://10athletes.netlify.app'
 else
   Rails.application.config.session_store :cookie_store, key: '_10-athletes'
 end
