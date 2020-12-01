42ba83203a6f97d88ff8b52fd6735bd257e3819c
Jon Mendelson <mendelsonja@gmail.com>
2020-11-30 18:46:52 -0800

    tweak sessions controller

---
+++ config/environment.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+# Load the Rails application.
+require_relative 'application'
+require 'active_support'
+require 'active_support/core_ext/hash/keys'
+
+
+# Initialize the Rails application.
+Rails.application.initialize!
