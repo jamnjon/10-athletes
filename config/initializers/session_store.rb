e2e9ec6423684a208b62fc508076d8ca9a2f6066
Jon Mendelson <mendelsonja@gmail.com>
2020-12-01 16:06:44 -0800

    tweaked from triple equal in sessionstore to .production?

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,4 +1,4 @@
-if Rails.env === 'production'
+if Rails.env.production?
   Rails.application.config.session_store :cookie_store, key: '_10-athletes', domain: '10athletes.netlify.app'
 else
   Rails.application.config.session_store :cookie_store, key: '_10-athletes'
