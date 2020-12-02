50eff4a52796605ce020b6d42d306fed7124bbbe
Jon Mendelson <mendelsonja@gmail.com>
2020-12-01 16:22:59 -0800

    added 10athletes.com to cors.rb

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -16,4 +16,13 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
       methods: [:get, :post, :put, :patch, :delete, :options, :head],
       credentials: true
   end
+
+  allow do
+    origins 'https://10athletes.com'
+
+    resource '*',
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head],
+      credentials: true
+  end
 end
